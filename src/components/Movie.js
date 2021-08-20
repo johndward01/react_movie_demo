d93cfbcdabbfb23392465036da623bdbe6f918eb
johndward01 <johndward01@gmail.com>
2021-08-19 21:39:54 -0500

    update

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,6 +7,9 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 // Components
 import Grid from './Grid';
 import Spinner from './Spinner';
+import BreadCrumb from './BreadCrumb';
+import MovieInfo from './MovieInfo';
+import MovieInfoBar from './MovieInfoBar';
 
 // Hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
@@ -19,9 +22,17 @@ import NoImage from '../images/no_image.jpg';
 const Movie = () => {
     const { movieId } = useParams();
     const { state: movie, loading, error } = useMovieFetch(movieId);
+    if (loading) return <Spinner />
+    if (error) return <div>Something went wrong...</div>;
     return (
         <>
-            <div>Movie</div>
+            <BreadCrumb movieTitle={movie._title} />
+            <MovieInfo movie={movie} />
+            <MovieInfoBar
+                time={movie.runtime}
+                budget={movie.budget}
+                revenue={movie.revenue}
+            />
         </>
     );
 };
